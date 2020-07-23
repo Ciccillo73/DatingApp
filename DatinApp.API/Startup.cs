70d5f30b7c2804ac7a008e080e84dffc5b84e507
Francesco <med.dama@gmail.com>
2020-07-23 13:55:30 +0100

    Comment out the app.UseHttpsRedirection(); function

--- DatinApp.API/Startup.cs
+++ DatinApp.API/Startup.cs
@@ -36,7 +36,7 @@ namespace DatinApp.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
